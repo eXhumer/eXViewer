b527939917013955d7b933546f1d211678a4bf95
eXhumer <exhumer1@protonmail.com>
2024-04-01 20:03:33 -0600

    chore: Add license information to source
    Signed-off-by: eXhumer <exhumer1@protonmail.com>

--- webpack/main.config.ts
+++ webpack/main.config.ts
@@ -1,15 +1,27 @@
+/*
+  eXViewer - Unofficial live timing and content streaming client for F1TV
+  Copyright (C) 2024  eXhumer
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, version 3 of the License.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import type { Configuration } from 'webpack';
 
 import { rules } from './rules';
 import { plugins } from './plugins';
 
 export const mainConfig: Configuration = {
-  /**
-   * This is the main entry point for your application, it's the first file
-   * that runs in the main process.
-   */
   entry: './src/Index.ts',
-  // Put your normal webpack config below here
   module: {
     rules,
   },
