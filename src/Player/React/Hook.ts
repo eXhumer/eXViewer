44cd452f85ca3fd1a904953e416954257583089d
eXhumer <exhumer1@protonmail.com>
2024-05-14 11:03:05 -0600

    chore: Add license boilerplate to source
    Signed-off-by: eXhumer <exhumer1@protonmail.com>

--- src/Player/React/Hook.ts
+++ src/Player/React/Hook.ts
@@ -1,3 +1,20 @@
+/*
+  eXViewer - Unofficial live timing and content streaming client for F1TV
+  Copyright (C) 2024  eXhumer
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, version 3 of the License.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { useDispatch, useSelector } from 'react-redux';
 import type { AppDispatch, RootState } from './Store';
 
