c3a86cee8c07b000d1d6b208f977d596338db551
eXhumer <exhumer@exhumer.cc>
2024-11-07 16:09:26 -0700

    chore: small changes * update default eslint config to disable various rules as per typescript-eslint recommendation * add new eslint config to run with ci * add script to package.json to run CI specific linting * update CI to lint first and only run build on successful linting
    Signed-off-by: eXhumer <exhumer@exhumer.cc>

--- eslint.config.ci.mjs
+++ eslint.config.ci.mjs
@@ -21,10 +21,6 @@ export default [
       'import/default': 'off',
       'import/no-named-as-default-member': 'off',
       'import/no-unresolved': 'off',
-      'import/no-named-as-default': 'off',
-      'import/no-cycle': 'off',
-      'import/no-unused-modules': 'off',
-      'import/no-deprecated': 'off',
     },
   },
 ];
